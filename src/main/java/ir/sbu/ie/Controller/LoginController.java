e2739354ee0d29b4ad83d20dca46344ceaa50d57
Masoumeh Kouhestani <m.kouhestani@mail.sbu.ac.ir>
2019-05-17 16:58:16 +0430

    sign up redirect

--- src/main/java/ir/sbu/ie/Controller/LoginController.java
+++ src/main/java/ir/sbu/ie/Controller/LoginController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.core.Context;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/login")
@@ -18,13 +24,22 @@ public class LoginController {
     @Inject
     public LoginService loginService;
 
+    @Context
+    public HttpServletRequest request;
+    @Context
+    public HttpServletResponse response;
+
     @GetMapping("/userlogin")
     @Consumes("application/x-www-form-urlencoded")
-    public boolean login(@FormParam("email") String email, @FormParam("password") String password) {
+    public boolean login(@FormParam("email") String email, @FormParam("password") String password) throws ServletException, IOException {
         User user = new User();
         user.setPassword(password);
         user.setEmail(email);
         boolean result = loginService.userLogin(user);
+
+//        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/orginal.jsp");
+//        dispatcher.forward(request, response);
+
         return result;
     }
 
--- src/main/java/ir/sbu/ie/Controller/SignUpController.java
+++ src/main/java/ir/sbu/ie/Controller/SignUpController.java
@@ -2,25 +2,49 @@ package ir.sbu.ie.Controller;
 
 import ir.sbu.ie.Entity.User;
 import ir.sbu.ie.Service.SignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 
-@RestController
+@Controller
 @RequestMapping("/signup")
 public class SignUpController {
     @Inject
     public SignUpService signUpService;
 
+//    @Context
+//    public HttpServletRequest request;
+//    @Context
+//    public HttpServletResponse response;
+
     @PostMapping("/usersignup")
     @Consumes("application/x-www-form-urlencoded")
-    public void signup(@FormParam("name") String name, @FormParam("username") String username, @FormParam("email") String email
-            , @FormParam("password") String password, @FormParam("position") String position, @FormParam("section") String section) {
+    @Produces("text/html")
+    public String signup(@FormParam("name") String name, @FormParam("username") String username, @FormParam("email") String email
+            , @FormParam("password") String password, @FormParam("position") String position, @FormParam("section") String section) throws ServletException, IOException {
         User user = new User();
         user.setName(name);
         user.setUsername(username);
@@ -29,5 +53,7 @@ public class SignUpController {
         user.setPosition(position);
         user.setSection(section);
         signUpService.userSignUp(user);
+
+        return "redirect:/orginal.html";
     }
 }
